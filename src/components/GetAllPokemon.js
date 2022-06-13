7adc5709d453cd929a6b51d23b45d99f7f65c061
Dyllan Moore <dyllantmoore@gmail.com>
2022-06-13 13:08:48 -0500

    :(

--- src/components/GetAllPokemon.js
+++ src/components/GetAllPokemon.js
@@ -1,20 +1,24 @@
 import Axios from "axios";
+import { useState } from 'react';
+
 export function GetAllPokemon() {
-    const pokemonArray = [];
+    const pokemonArray = [useState("")];
+
 
     Axios.get('https://pokeapi.co/api/v2/pokemon?limit=4')
         .then(function (response) {
             pokemonArray.push(...response.data.results)
-            return (
-                pokemonArray.map((pokemon) => (
+            pokemonArray.map((pokemon) => {
+                return (
                     <div className="PokemonListContainer">
                         <h2 className="PokemonName">
                             Name: {pokemon.name}
                             {console.log(pokemon.name)}
                         </h2>
                     </div>
-                ))
-            )
+                )
+            })
+
         })
 
 }
